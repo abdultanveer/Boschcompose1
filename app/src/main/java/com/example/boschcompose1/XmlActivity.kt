b9b1805458caef3de44fb981d796aac03bbe85fa
abdul <abdul.tanveer@gmail.com>
2025-03-19 15:57:13 +0530

    requesting runtime permissions

--- app/src/main/java/com/example/boschcompose1/XmlActivity.kt
+++ app/src/main/java/com/example/boschcompose1/XmlActivity.kt
@@ -1,6 +1,7 @@
 package com.example.boschcompose1
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
@@ -8,8 +9,11 @@ import android.util.Log
 import android.view.View
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 
@@ -17,24 +21,59 @@ class XmlActivity : AppCompatActivity() {
     var TAG = XmlActivity::class.java.simpleName
     lateinit var cpListView:ListView
 
+    private val SMS_PERMISSION_CODE = 101 //request code
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_xml)
         cpListView = findViewById(R.id.listView)
 
-        val uriSms = Uri.parse("content://sms/inbox")
         //content resolver --bring up the db -- intent - activity via intent filter table
-        val cursor: Cursor? = getContentResolver().query(uriSms, null, null, null, null)
-        //select * from uri
-       var fromColumns = arrayOf("address","body")
-        var toTextViews = intArrayOf(android.R.id.text1,android.R.id.text2)
-
-        var myAdapter:SimpleCursorAdapter = SimpleCursorAdapter(this,
-            android.R.layout.simple_list_item_2,cursor,fromColumns,toTextViews)
-        cpListView.adapter = myAdapter
+       if( checkSmsPermission()) {
+          setResultListView()
+       }
+        else {
+            requestSmsPermission()
+        }
 
     }
+
+
+    private fun checkSmsPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestSmsPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(
+            android.Manifest.permission.READ_SMS),
+            SMS_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setResultListView()
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setResultListView() {
+        val uriSms = Uri.parse("content://sms/inbox")
+
+        val cursor: Cursor? = getContentResolver().query(uriSms, null, null, null, null)
+
+        //select * from uri
+        var fromColumns = arrayOf("address", "body")
+        var toTextViews = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        var myAdapter: SimpleCursorAdapter = SimpleCursorAdapter(
+            this,
+            android.R.layout.simple_list_item_2, cursor, fromColumns, toTextViews
+        )
+        cpListView.adapter = myAdapter    }
 
     fun clickHandler(view: View) {
         var view:ConstraintLayout = findViewById(R.id.conslayout)
